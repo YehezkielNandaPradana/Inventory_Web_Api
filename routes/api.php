1dc714b9f33d3e0e82d4507f40ce65b709e05df9
Naxx <yehezkieldaniela@gmail.com>
2026-07-24 13:26:18 +0700

    Add rekap, transaksi, and profile routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BarangController;
 use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\RekapController;
 use App\Http\Controllers\Api\StokMovementController;
+use App\Http\Controllers\Api\TransaksiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', function () {
@@ -11,11 +13,17 @@ Route::get('/health', function () {
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/profile', [AuthController::class, 'profile']);
 
 Route::apiResource('kategoris', KategoriController::class);
 Route::apiResource('barangs', BarangController::class);
+
+Route::get('/barangs/{barang}/history', [BarangController::class, 'history']);
+
+Route::apiResource('transaksi', TransaksiController::class)->only(['index', 'store']);
+
+Route::get('/rekap', [RekapController::class]);
+
 Route::apiResource('stok-movements', StokMovementController::class)->except(['update']);
 
 Route::patch('stok-movements/{stok_movement}', [StokMovementController::class, 'update']);
-
-Route::get('barangs/{barang}/history', [BarangController::class, 'history']);
